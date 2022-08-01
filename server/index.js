c5959e0046bd16363001451da6a479a05d1f0083
C <caaarloos300@gmail.com>
2022-08-01 06:28:18 +0200

    solving disputes

---
+++ server/index.js
@@ -0,0 +1,72 @@
+// const express = require('express');
+// const db = require('./config/db')
+// const cors = require('cors')
+
+// const app = express();
+
+// const  PORT = 3002;
+// app.use(cors());
+// app.use(express.json())
+
+// Route to get all posts
+// app.get("/api/get", (req,res)=>{
+// db.query("SELECT * FROM posts", (err,result)=>{
+//     if(err) {
+//     console.log(err)
+//     } 
+// res.send(result)
+// }
+//     );   
+// });
+
+// Route to get one post
+// app.get("/api/getFromId/:id", (req,res)=>{
+
+// const id = req.params.id;
+//     db.query("SELECT * FROM posts WHERE id = ?", id, (err,result)=>{
+//         if(err) {
+//         console.log(err)
+//         } 
+//     res.send(result)
+//     }
+//     );   
+//     });
+
+// Route for creating the post
+// app.post('/api/create', (req,res)=> {
+
+// const username = req.body.userName;
+// const title = req.body.title;
+// const text = req.body.text;
+
+// console.log(username,title,text)
+
+// db.query("INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",[title,text,username], (err,result)=>{
+//    if(err) {
+//        console.log(err)
+//    } 
+//    console.log(result)
+// }
+// );   
+// })
+
+// Route for like
+
+
+
+// app.listen(PORT, ()=>{
+//     console.log(`Server is running on ${PORT}`)
+// })
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+//   });
+  
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
